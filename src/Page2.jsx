5c89bc4ee76be2d0ef2ea461ce3bf127d12e2542
asa129 <asa.jad1290@gmail.com>
2025-03-23 18:43:09 +0900

    クエリパラメータを表示する画面を追加

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -8,6 +8,8 @@ export const Page2 = () => {
       <Link to="./detailA">DetailAへ</Link>
       <br />
       <Link to="./100">Urlパラメータへ</Link>
+      <br />
+      <Link to="./100">クエリパラメータへ</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useSearchParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const [query] = useSearchParams(search);
   return (
     <div>
       <h1>URLパラメーターページです</h1>
       <p>{`idは${id}だよ`}</p>
+      <p>{`パラメータは${query.get("name")}だよ`}</p>
     </div>
   );
 };
