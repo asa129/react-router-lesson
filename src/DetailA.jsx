9a6ca8e98053efb09dcf6dd04a0f71343400b294
asa129 <asa.jad1290@gmail.com>
2025-03-23 20:20:28 +0900

    遷移時にステートを上げるように修正、リンク以外の遷移を追加

--- src/DetailA.jsx
+++ src/DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 export const DetailA = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location.state);
+  const onClickBack = () => navigate(-1);
   return (
     <div>
       <h1>DetailAページです</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
+  const array = [...Array(100).keys()];
+  const navigate = useNavigate();
+  const onClickDetailA = () => {
+    navigate("./detailA");
+  };
+
   return (
     <div>
       <h1>Page1ページです</h1>
       <Outlet />
-      <Link to="./detailA">DetailAへ</Link>
+      <Link to="./detailA" state={{ array: array }}>
+        DetailAへ
+      </Link>
       <br />
       <Link to="./detailB">DetailBへ</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailAへ</button>
     </div>
   );
 };
